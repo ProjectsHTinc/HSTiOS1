e5e05193c7ea094c19172e8389c2c6f0987f45a6
iRarity <balapadmanabhan93@gmail.com>
2021-01-29 13:05:01 +0530

    jan 29 2021 issue fixed

--- SkilEx/MainViewControllers/CustomerAddress.swift
+++ SkilEx/MainViewControllers/CustomerAddress.swift
@@ -658,12 +658,12 @@ class CustomerAddress: UIViewController, CLLocationManagerDelegate, UIGestureRec
     
     func calculateDistance (startLocation: CLLocation, EndLocation:CLLocation){
       
-        let distanceKM: CLLocationDistance  = (startLocation.distance(from: EndLocation))  / 1000.0
-        //let distance =  Int(distanceKM )
+        let distanceKM: CLLocationDistance  = (startLocation.distance(from: EndLocation))
+        let distance =  Int(distanceKM / 1000.0)
         //print("Test\(distanceMeters)")
-        print(distanceKM)
-        let distance = String(format: "%.01f", distanceKM)
-        if distance <= "20.0"{
+        //print(distanceKM)
+        //let distance = String(format: "%.01f", distanceKM)
+        if distance <= 20 {
             availableDistance = true
         }
         else{
